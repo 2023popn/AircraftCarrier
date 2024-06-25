561b270914ad43a433bde4a81bb36a4463099e3d
2023popn <nathanp2023@gmail.com>
2024-06-24 22:04:29 -0400

    Update to map, capitalization, declaration
    setspeed changed to setSpeed for consistency. GeneralMap function added in new header file (https://forum.arduino.cc/t/arduino-map-function-for-float-values/112888/6). Constants moved to preprocessor directives for global use. Added empty function to calculate speed

--- Catapault/Catapault.ino
+++ Catapault/Catapault.ino
@@ -1,29 +1,31 @@
 #include <ESP32Servo.h>
+#include <GeneralMap.h>
 
 Servo servo1;
+
+#define targetVelocity 17
+#define maxPWM 180
 
 void setup() {
   // Set pin mode
   servo1.attach(4);
   servo1.write(0);
-
-  // Speed control variables
-  const int maxPWM = 180;
-  const float targetVelocity = 17;
 }
 
 void loop() {
   delay(1000);
-  setspeed(180);
+  setSpeed(17);
   delay(1000);
-  setspeed(0);
+  setSpeed(0);
 }
 
 
-void setspeed(int speed) {
-  int signal = map(speed, 0, targetVelocity, 0, maxPWM); // Remaps a percentage of max speed (0-100) to a servo PWM signal (0-180)
+void setSpeed(float speed) {
+  int signal = mapGeneric(speed, 0, targetVelocity, 0, maxPWM); // Remaps a percentage of max speed (0-100) to a servo PWM signal (0-180)
   
   servo1.write(signal);
 }
 
+float getCurrentSpeed(unsigned long time){
 
+}
